fe1fd8fe1f4db01ea831d116e08f6f2e8172d6cd
BandsWithLegends <adamosimpson21@gmail.com>
2018-12-30 21:29:55 -0800

    Hiding password unnecessarily

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const tmi = require("tmi.js");
 const {password} = require("./password");
 
 // const thirtyDaysInMilliseconds = 2.592e+9;
+// const oneHourInMilliseconds = 3.6e+6;
+
+//testing times
 const thirtyDaysInMilliseconds = 1000*60;
 const oneHourInMilliseconds = 1000*5;
-// const oneHourInMilliseconds = 3.6e+6;
+
 const database = {};
 
 const options = {
@@ -31,6 +34,7 @@ client.connect();
 client.on("chat", (channel, user, message, self) => {
   if(self) return;
   const {username} = user
+  // if(method==="prime"){
   if(message==="prime" && username==='bandswithlegends'){
     if(database[username] && database[username].blacklist) return null;
     else if(database[username] && !database[username].blacklist){
@@ -39,7 +43,6 @@ client.on("chat", (channel, user, message, self) => {
       database[username].channel = channel;
     } else {
       database[username] = {username, channel, lastSubbed: Date.now(), blacklist: false}
-      console.log("database:", database)
       client.whisper(database[username].username, "Hello, You subbed with Twitch Prime. If you'd like, I'll message you in 30 days to remind you to sub with Twitch Prime again. If not, simply message me back !stop and you will not receive any more messages from me.");
     }
   }
@@ -48,22 +51,20 @@ client.on("chat", (channel, user, message, self) => {
 client.on("whisper", (from, userstate, message, self) => {
   if(self) return;
   const {username} = userstate
-  if(message === '!stop'){
-    if(database[username]){
-      database[username].blacklist = true;
-      client.whisper(username, "Service Stopped. If you'd like to restart service message me !start ")
-    } else {
-      client.whisper(username, "Sorry, couldn't find you in the database. Subscribe with Twitch Prime to begin service")
-    }
-  } else if(message ==='!start'){
-    if(database[username]){
-      database[username].blacklist = false;
-      client.whisper(username, "Service started")
-    } else {
-      client.whisper(username, "Sorry, couldn't find you in the database. Subscribe with Twitch Prime to begin service")
-    }
+  if(!database[username]){
+    client.whisper(username, "Sorry, couldn't find you in the database. Subscribe with Twitch Prime to begin service")
   } else {
-    client.whisper(username, "Sorry, couldn't understand your message. Use !stop or !start to update service")
+    if(message === '!stop'){
+        database[username].blacklist = true;
+        client.whisper(username, "Service Stopped. If you'd like to restart service message me !start ")
+    } else if(message ==='!start'){
+      if(database[username]){
+        database[username].blacklist = false;
+        client.whisper(username, "Service started")
+      }
+    } else {
+      client.whisper(username, "Sorry, couldn't understand your message. Use !stop or !start to update service")
+    }
   }
 })
 
@@ -75,11 +76,8 @@ const formatChannelName = channel => channel[0] ==="#" ? channel.slice(1) : chan
 
 // every hour check to see whose re-subscription is available
 function checkDatabase(){
-  console.log("checking database:", database)
   Object.entries(database).forEach(users => {
-    console.log("users: ", users)
     const {blacklist, username, channel, lastSubbed } = users[1];
-    console.log("info:", blacklist, username, channel, lastSubbed)
     if(!blacklist && oneMonthOld(lastSubbed)){
       const channelName = formatChannelName(channel);
       client.whisper(username, `Hey ${username}, your twitch prime sub is available. Last month you subbed to ${channelName}. If you'd like to sub to them again go to https://www.twitch.tv/${channelName} `)
