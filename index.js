e6c5bed894cf1aa772a444b95dca2565a84822a7
BandsWithLegends <adamosimpson21@gmail.com>
2019-01-05 22:28:55 -0800

    testing out port variable

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const oneHourInMilliseconds = 1000*5;
 
 // possible messages
 const unknownMessage = "Sorry, couldn't understand your message. Use !info, !contact, !start, or !stop";
-const port = process.env.PORT
+const port = process.env.PORT;
 
 //connecting to twitch client
 const options = {
@@ -19,7 +19,7 @@ const options = {
     debug:true
   },
   connection:{
-    port: port,
+    port: 2341,
     reconnect:true
   },
   identity:{
